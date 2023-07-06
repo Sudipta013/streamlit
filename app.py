48c8cfd0dfdf0e716846f499c5d7f4c5313e8115
Sudipta paul <Sudiptopaul403@gmail.com>
2023-07-06 14:11:06 +0530

    app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def main():
         chunks = text_splitter.split_text(text)
 
         # create embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key = key)
+        embeddings = OpenAIEmbeddings(openai_api_key=apikey)
         knowledge_base = FAISS.from_texts(chunks, embeddings)
 
         # show user input
@@ -58,7 +58,7 @@ def main():
         if user_question:
             docs = knowledge_base.similarity_search(user_question)
 
-            llm = OpenAI(openai_api_key = key)
+            llm = OpenAI(openai_api_key=apikey)
             chain = load_qa_chain(llm, chain_type="stuff")
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=user_question)
