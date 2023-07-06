cfdde3a00450f2757a2fbd11e6f927a1aab1a15e
Sudipta paul <Sudiptopaul403@gmail.com>
2023-07-06 13:49:01 +0530

    app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 import os
+import openai
 
 headers = {
     "authorization": st.secrets["auth_token"],
